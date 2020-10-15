71c8c71c9548f866c8f0f12e302938df8650b624
jlhedenskog <jessehka@gmail.com>
2020-10-15 12:49:38 -0400

    Initial commit

---
+++ FizzBuzz-Core-Using-Models/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FizzBuzz_Core_Using_Models.Models;
+
+namespace FizzBuzz_Core_Using_Models.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult FizzBuzz()
+        {
+            var model = new FizzBuzzModel();
+            model.Output = new List<string>();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult FizzBuzz(int userNumber1, int userNumber2)           
+        {
+            var output = "";
+            
+            for (var i = 1; i <= 100; i++)
+            {
+                var fizzBug = i % userNumber1;
+                var buzzBug = i % userNumber2;
+
+                if (fizzBug == 0 && buzzBug == 0)
+                {
+                    output += "FizzBuzz ";
+                }
+                else if (fizzBug == 0)
+                {
+                    output += "Fizz ";
+                }
+                else if (buzzBug == 0)
+                {
+                    output += "Buzz ";
+                }
+                else
+                {
+                    output += i + " ";
+                }
+            }
+
+            var fizzBuzzModel = new FizzBuzzModel();
+            fizzBuzzModel.Fizzer = userNumber1;
+            fizzBuzzModel.Buzzer = userNumber2;
+            fizzBuzzModel.Output = output.Split(" ").ToList();
+
+
+            return View(fizzBuzzModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FizzBuzz-Core-Using-Models/Models/FizzBuzzModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_Core_Using_Models.Models
+{
+    public class FizzBuzzModel
+    {
+        public int Fizzer { get; set; }
+        public int Buzzer { get; set; }
+        public List<string> Output { get; set; }
+    }
+}
